f7022750d29bdf5875a21dcc4116f3f28dbb8f9c
Siriphatson Sringamphong <6410685249@cn351>
2024-04-01 23:21:19 +0700

    Add path

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res) => {
-    res.send('Started Working, Express!');
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
 });
 
 app.listen(8888, () => {
-    console.log('http://localhost:8888');
+    console.log('Click URL: http://localhost:8888');
 });
